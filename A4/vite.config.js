059a7624bec0ba9e50817a1ff1a1c7148f18cff5
cwsmith <cwsmith@wpi.edu>
2025-02-19 00:06:28 -0500

    Removed necessary imports

--- A4/vite.config.js
+++ A4/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowHosts: true,
+    allowedHosts: "a4-christophersmith101.onrender.com",
   }
 })
